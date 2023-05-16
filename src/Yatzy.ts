4f7a521c201359f14a9d975a81e7f2d041ef4625
Guillaume <mathieu.guillaume93@gmail.com>
2023-05-16 02:50:20 +0200

    straights

--- src/Yatzy.ts
+++ src/Yatzy.ts
@@ -10,13 +10,7 @@ export default class Yatzy {
     }
 
     static yatzy(...args: number[]): number {
-        var counts = [0, 0, 0, 0, 0, 0, 0, 0];
-        for (var i = 0; i != args.length; ++i) {
-            var die = args[i];
-            counts[die - 1]++;
-        }
-        for (i = 0; i != 6; i++) if (counts[i] == 5) return 50;
-        return 0;
+        return this.minOccurrence(args, 5).length > 0 ? 50 : 0;
     }
 
     static ones(...dice: number[]): number {
@@ -92,59 +86,35 @@ export default class Yatzy {
         return triple.length === 0 ? 0 : triple[0] * 3;
     }
 
-    static smallStraight(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-        var tallies;
-        tallies = [0, 0, 0, 0, 0, 0, 0];
-        tallies[d1 - 1] += 1;
-        tallies[d2 - 1] += 1;
-        tallies[d3 - 1] += 1;
-        tallies[d4 - 1] += 1;
-        tallies[d5 - 1] += 1;
-        if (tallies[0] == 1 && tallies[1] == 1 && tallies[2] == 1 && tallies[3] == 1 && tallies[4] == 1) return 15;
-        return 0;
+    static straight(dice: number[], start: number, end: number) {
+        const freq = this.freq(dice);
+        for (let die = start; die <= end; die++) {
+            const dc = freq.find(d => d.die === die);
+            if (!dc || dc.count === 0) {
+                return 0;
+            }
+        }
+        // The sum of an arithmetic progression
+        return ((end - start + 1) * (start + end)) / 2;
     }
 
-    static largeStraight(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-        var tallies;
-        tallies = [0, 0, 0, 0, 0, 0, 0, 0];
-        tallies[d1 - 1] += 1;
-        tallies[d2 - 1] += 1;
-        tallies[d3 - 1] += 1;
-        tallies[d4 - 1] += 1;
-        tallies[d5 - 1] += 1;
-        if (tallies[1] == 1 && tallies[2] == 1 && tallies[3] == 1 && tallies[4] == 1 && tallies[5] == 1) return 20;
-        return 0;
+    static smallStraight(...dice: number[]): number {
+        return this.straight(dice, 1, 5);
     }
 
-    static fullHouse(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-        var tallies;
-        var _2 = false;
-        var i;
-        var _2_at = 0;
-        var _3 = false;
-        var _3_at = 0;
+    static largeStraight(...dice: number[]): number {
+        return this.straight(dice, 2, 6);
+    }
 
-        tallies = [0, 0, 0, 0, 0, 0, 0, 0];
-        tallies[d1 - 1] += 1;
-        tallies[d2 - 1] += 1;
-        tallies[d3 - 1] += 1;
-        tallies[d4 - 1] += 1;
-        tallies[d5 - 1] += 1;
-
-        for (i = 0; i != 6; i += 1)
-            if (tallies[i] == 2) {
-                _2 = true;
-                _2_at = i + 1;
-            }
-
-        for (i = 0; i != 6; i += 1)
-            if (tallies[i] == 3) {
-                _3 = true;
-                _3_at = i + 1;
-            }
-
-        if (_2 && _3) return _2_at * 2 + _3_at * 3;
-        else return 0;
+    static fullHouse(...dice: number[]): number {
+        const freq = this.freq(dice);
+        const d2 = freq.find(d => d.count === 2);
+        const d3 = freq.find(d => d.count === 3);
+        if (d2 && d3) {
+            return d2.die * 2 + d3.die * 3;
+        } else {
+            return 0;
+        }
     }
 
 
